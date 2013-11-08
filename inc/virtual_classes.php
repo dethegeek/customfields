71e89ca7ebd2c21f2bccca56ff77aea502912aa4
dethegeek <dethegeek@gmail.com>
2013-11-08 09:02:36 +0100

    Reduced code in virtual class

--- inc/virtual_classes.php
+++ inc/virtual_classes.php
@@ -13,98 +13,99 @@ while ($data=$DB->fetch_assoc($result)) {
 	function getTabNameForItem(CommonGLPI $item, $withtemplate=0) {
 		global $LANG;
       	
-      	// These if blocks may appear useless. Let us keep them while I discover the plugin
-		if ($item->getType() == "Computer") {
-			return $LANG["plugin_customfields"]["title"];
-		}
-		if ($item->getType() == "Monitor") {
-			return $LANG["plugin_customfields"]["title"];
-		}
-		if ($item->getType() == "Software") {
-			return $LANG["plugin_customfields"]["title"];
-		}
-		if ($item->getType() == "NetworkEquipment") {
-			return $LANG["plugin_customfields"]["title"];
-		}
-		if ($item->getType() == "Peripheral") {
-			return $LANG["plugin_customfields"]["title"];
-		}
-      	if ($item->getType() == "Printer") {
-			return $LANG["plugin_customfields"]["title"];
-		}
-		if ($item->getType() == "CartridgeItem") {
-			return $LANG["plugin_customfields"]["title"];
-		}
-		if ($item->getType() == "ConsumableItem") {
-			return $LANG["plugin_customfields"]["title"];
-		}
-		if ($item->getType() == "Phone") {
-			return $LANG["plugin_customfields"]["title"];
-		}
+		switch ($item->getType()) {
+      		case "Computer":
+      		case "Monitor":
+      		case "Software":
+      		case "NetworkEquipment":
+      		case "Peripheral":
+      		case "Printer":
+      		case "CartridgeItem":
+      		case "ConsumableItem":
+      		case "Phone":
+      		case "ComputerDisk":
+      		case "Supplier":
+				return $LANG["plugin_customfields"]["title"];
+      			break;
+      	}
       		
       	return "";
 	}
 	
 	static function displayTabContentForItem(CommonGLPI $item, $tabnum=1, $withtemplate=0) {
 
+      	// TODO: Rename $prof into something more explicit 
+      	// (the name comes from a copy / paste https://forge.indepnet.net/projects/plugins/wiki/Fr_CreatePlugin084)
 		switch ($item->getType()) {
-      		case "Computer":
-        		$prof = new self();
-         		$ID = $item->getField("id");
-        		// j affiche le formulaire
-         		$prof->showForm($ID);
-      			break;
-	      	case "Monitor":
-		         $prof = new self();
-		         $ID = $item->getField("id");
-		        // j affiche le formulaire
-		         $prof->showForm($ID);
-      			break;
-	      	case "Software":
-		         $prof = new self();
-		         $ID = $item->getField("id");
-		        // j affiche le formulaire
-		         $prof->showForm($ID);
-      			break;
-   	      	case "NetworkEquipment":
-		         $prof = new self();
-		         $ID = $item->getField("id");
-		        // j affiche le formulaire
-		         $prof->showForm($ID);
-      			break;
-   	      	case "Peripheral":
-		         $prof = new self();
-		         $ID = $item->getField("id");
-		        // j affiche le formulaire
-		         $prof->showForm($ID);
-      			break;
-   	      	case "Printer":
-		         $prof = new self();
-		         $ID = $item->getField("id");
-		        // j affiche le formulaire
-		         $prof->showForm($ID);
-      			break;
-   	      	case "CartridgeItem":
-		         $prof = new self();
-		         $ID = $item->getField("id");
-		        // j affiche le formulaire
-		         $prof->showForm($ID);
-      			break;
-   	      	case "ConsumableItem":
-		         $prof = new self();
-		         $ID = $item->getField("id");
-		        // j affiche le formulaire
-		         $prof->showForm($ID);
-      			break;
-   	      	case "Phone":
-		         $prof = new self();
-		         $ID = $item->getField("id");
-		        // j affiche le formulaire
-		         $prof->showForm($ID);
-      			break;
-   }
-      // TODO: Check if we must always return true 
-      return true;
+			case "Computer":
+				$prof = new self();
+				$ID = $item->getField("id");
+				// j affiche le formulaire
+				$prof->showForm($ID);
+				break;
+			case "Monitor":
+				$prof = new self();
+				$ID = $item->getField("id");
+				// j affiche le formulaire
+				$prof->showForm($ID);
+				break;
+			case "Software":
+				$prof = new self();
+				$ID = $item->getField("id");
+				// j affiche le formulaire
+				$prof->showForm($ID);
+				break;
+			case "NetworkEquipment":
+				$prof = new self();
+				$ID = $item->getField("id");
+				// j affiche le formulaire
+				$prof->showForm($ID);
+				break;
+			case "Peripheral":
+				$prof = new self();
+				$ID = $item->getField("id");
+				// j affiche le formulaire
+				$prof->showForm($ID);
+				break;
+			case "Printer":
+				$prof = new self();
+				$ID = $item->getField("id");
+				// j affiche le formulaire
+				$prof->showForm($ID);
+				break;
+			case "CartridgeItem":
+				$prof = new self();
+				$ID = $item->getField("id");
+				// j affiche le formulaire
+				$prof->showForm($ID);
+				break;
+			case "ConsumableItem":
+				$prof = new self();
+				$ID = $item->getField("id");
+				// j affiche le formulaire
+				$prof->showForm($ID);
+				break;
+			case "Phone":
+				$prof = new self();
+				$ID = $item->getField("id");
+				// j affiche le formulaire
+				$prof->showForm($ID);
+				break;
+			case "ComputerDisk":
+				$prof = new self();
+				$ID = $item->getField("id");
+				// j affiche le formulaire
+				$prof->showForm($ID);
+				break;
+			case "Supplier":
+				$prof = new self();
+				$ID = $item->getField("id");
+				// j affiche le formulaire
+				$prof->showForm($ID);
+				break;
+		}
+		// TODO: Check if we must always return true 
+		return true;
    }
       		
    function showForm($ID, $options=array()) {
